a8d5e31c9bdec3094e46365b2c3e664211c85db3
RobinDavid <dev.robin.david@gmail.com>
2021-07-30 10:19:27 +0200

    expose classe & modules in qsynthesis directly

--- qsynthesis/__init__.py
+++ qsynthesis/__init__.py
@@ -1,5 +1,10 @@
+from qsynthesis.algorithms import TopDownSynthesizer, PlaceHolderSynthesizer
+from qsynthesis.grammar import TritonGrammar, BvOp
+from qsynthesis.tables import LookupTableLevelDB, LookupTableREST
+from qsynthesis.utils.symexec import SimpleSymExec
+from qsynthesis.tritonast import TritonAst
 
 __version__ = "0.1"
 
-
+# Simple object used to retrieve the plugin in IDA
 qsynthesis_plugin = None
